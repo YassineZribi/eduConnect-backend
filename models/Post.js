e27629ecf5553dc3ffca8925d5fac2cf0bf97822
YassineZribi <yassinezribiyassine@gmail.com>
2020-05-29 12:13:51 +0100

    add another features

---
+++ models/Post.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+    teamMember: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "TeamMember"
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    imagesAndVideos: [
+        {
+            type: String
+        }
+    ],
+    childhoodInstitution: {                     // .populate("childhoodInstitution", ["institutionName", "logo"])
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "ChildhoodInstitution"
+    },
+    likes: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                refPath: "onModel"
+            },
+            onModel: {
+                type: String,
+                required: true,
+                enum: ["Parent", "TeamMember"]
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                refPath: "onModel"
+            },
+            onModel: {
+                type: String,
+                required: true,
+                enum: ["Parent", "TeamMember"]
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+module.exports = mongoose.model("Post", PostSchema);
+
+
+
--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const authPrivRoutes = require("../middleware/authPrivRoutes");
+const Post = require("../models/Post");
+// const Profile = require("../models/Profile");
+// const User = require("../models/User");
+const TeamMember = require("../models/TeamMember");
+const createPostValidation = require("./validations/createPostValidation");
+const checkForHexRegExpFunction = require("./validations/checkMongodbIdValidity");
+
+
+
 
 
 // @route   *** GET /posts ***
@@ -9,4 +19,47 @@ router.get("/", (req, res) => {
     res.send("Posts route");
 });
 
+// @route   *** POST /posts ***
+// @desc    *** Create a post ***
+// @access  *** Private ***
+router.post("/:childhoodInstitutionId", authPrivRoutes, async (req, res) => {
+    try {
+        const userToAccess = await TeamMember.findOne({ _id: req.user.id, isVisible: true, isAccepted: true, isAllowed: true }); // .populate("childhoodInstitution", ["institutionName", "logo"])
+        if (!userToAccess) return res.status(403).json({ accessError: "Can not access this data" });
+
+
+        // access only for TeamMembers (only manager )
+        if (userToAccess.status.includes("manager") && userToAccess.childhoodInstitution._id == req.params.childhoodInstitutionId) {
+            const { error, value } = createPostValidation(req.body);
+
+            if (error) {
+                // console.log("error ", error);
+                // console.log('error:: ', JSON.stringify(error, null, 2));
+                console.log(JSON.stringify(error.details.map(obj => ({ [obj.context.key]: obj.message })).reduce((acc, cV) => ({ ...acc, ...cV }), {}), null, 2));
+                return res.status(400).json(error.details.map(obj => ({ [obj.context.key]: obj.message })).reduce((acc, cV) => ({ ...acc, ...cV }), {}));
+            }
+
+
+            // const teamMember = await TeamMember.findById(req.user.id, "-password");
+            // if (!teamMember) return res.status(404).json({ errorMsg: "Can not find User" });
+
+            const newPost = {
+                teamMember: req.user.id,
+                text: req.body.text,
+                imagesAndVideos: req.body.imagesAndVideos,
+                childhoodInstitution: req.user.childhoodInstitution
+            };
+
+
+            const post = new Post(newPost);
+            post.save();
+            res.json(post);
+
+        } else return res.status(403).json({ accessError: "Can not access this data (handle access)" });
+    } catch (err) {
+        console.error("error:: ", err.message);
+        res.status(500).json({ errorMsg: "server Error" });
+    }
+
+});
 module.exports = router;
